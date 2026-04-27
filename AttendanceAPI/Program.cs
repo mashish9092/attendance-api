93388d47b79aad30b1583c74a74c5a2f844ad4ff
Ashish Mehra <mashish9092@gmail.com>
2026-04-27 13:06:32 +0530

    fix swagger production

--- AttendanceAPI/Program.cs
+++ AttendanceAPI/Program.cs
@@ -1,16 +1,16 @@
-using AttendanceAPI.Data;
+﻿using AttendanceAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -22,12 +22,16 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+// ❌ REMOVE this पूरी if condition
+// if (app.Environment.IsDevelopment())
+
+// ✅ ADD THIS (ALWAYS ENABLE SWAGGER)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
